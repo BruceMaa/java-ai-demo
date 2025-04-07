a29e99911a3eccece5f6df3da8e30205252e3e48
maqiang9527 <maqiang_9527@163.com>
2025-04-07 16:37:55 +0800

    feat: ✨ 添加dashscope模型，简单对话

--- spring-ai-demo/src/main/java/cn/brucemaa/ai/demo/controller/ChatController.java
+++ spring-ai-demo/src/main/java/cn/brucemaa/ai/demo/controller/ChatController.java
@@ -1,5 +1,10 @@
 package cn.brucemaa.ai.demo.controller;
 
+import com.alibaba.cloud.ai.dashscope.chat.DashScopeChatOptions;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +14,38 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1")
 public class ChatController {
 
+    private static final String DEFAULT_PROMPT = "你是一个博学的智能聊天助手，请根据用户提问回答！";
+
+    private final ChatClient dashScopeChatClient;
+
+    public ChatController(ChatClient.Builder chatClientBuilder) {
+        this.dashScopeChatClient = chatClientBuilder
+                .defaultSystem(DEFAULT_PROMPT)
+                // 实现 Chat Memory 的 Advisor
+                // 在使用 Chat Memory 时，需要指定对话 ID，以便 Spring AI 处理上下文。
+                .defaultAdvisors(
+                        new MessageChatMemoryAdvisor(new InMemoryChatMemory())
+                )
+                // 实现 Logger 的 Advisor
+                .defaultAdvisors(
+                        new SimpleLoggerAdvisor()
+                )
+                // 设置 ChatClient 中 ChatModel 的 Options 参数
+                .defaultOptions(
+                        DashScopeChatOptions.builder()
+                                .withTopP(0.7)
+                                .build()
+                )
+                .build();
+    }
+
     @GetMapping
     public ResponseEntity<String> home() {
         return ResponseEntity.ok("Hello, World!");
+    }
+
+    @GetMapping("/simple/chat")
+    public String simpleChat(String query) {
+        return dashScopeChatClient.prompt(query).call().content();
     }
 }
